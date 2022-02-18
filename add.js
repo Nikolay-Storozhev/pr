98cec5a7bd247cec875c357785bad70d3922ce67
Nikolay-Storozhev <kolia.storozhiev@mail.ru>
2022-02-18 23:02:22 +0700

    попытка №1 вставить текст чрез input

--- add.js
+++ add.js
@@ -1,12 +1,15 @@
 function _clear(){
     const input = document.getElementById ('name').value = '';
-
-    console.log('el1')
 }
 
 let form = document.getElementById('back');
 form.addEventListener('submit', function(event){
     event.preventDefault();
     const input = document.getElementById ('name').value = '';
-    console.log('el2')
 })
+
+let input = document.getElementById('tx');
+input.insertAdjacentHTML('afterend', '<div class="taxt">rrr</div>');
+
+
+console.log(input) 
