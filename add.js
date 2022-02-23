2bc9e374782d6aabb5368804f8552459d357b508
Дмитрй Иванов <d.ivanov@bothelp.io>
2022-02-23 23:31:18 +0700

    Рабочая версия добавления задач

--- add.js
+++ add.js
@@ -1,15 +1,10 @@
-function _clear(){
-    const input = document.getElementById ('name').value = '';
-}
-
 let form = document.getElementById('back');
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    const input = document.getElementById ('name').value = '';
+    const input = document.getElementById('name').value;
+
+    let tx = document.getElementById('tx');
+    tx.insertAdjacentHTML('beforeend', '<div class="taxt">'+input+'</div>');
+
+    input.value = ''
 })
-
-let input = document.getElementById('tx');
-input.insertAdjacentHTML('afterend', '<div class="taxt">rrr</div>');
-
-
-console.log(input) 
