5e01faf530608891c1108d18f40dbac3094de19e
Дмитрй Иванов <d.ivanov@bothelp.io>
2022-02-23 23:41:58 +0700

    Поправил отчиску текста при submit формы

--- add.js
+++ add.js
@@ -1,10 +1,10 @@
 let form = document.getElementById('back');
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    const input = document.getElementById('name').value;
+    const input = document.getElementById('name');
 
     let tx = document.getElementById('tx');
-    tx.insertAdjacentHTML('beforeend', '<div class="taxt">'+input+'</div>');
+    tx.insertAdjacentHTML('beforeend', '<div class="taxt">'+input.value+'</div>');
 
     input.value = ''
 })
